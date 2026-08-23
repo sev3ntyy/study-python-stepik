a1c9e8ef19bfddb680126b1da9d7bce01cbf648c
seventy <sayni1434@gmail.com>
2026-08-23 15:25:25 +0700

    2 задагте

--- opp/opp2.py
+++ opp/opp2.py
@@ -3,3 +3,9 @@ class Cat:
         self.state = "спит"
     def get_state(self):
         return self.state
+
+class Book:
+    def __init__(self,title):
+        self.title = title
+    def get_title(self):
+        return self.title    
