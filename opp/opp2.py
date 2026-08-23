be14f9599626789abaa23c8535695e370e1f858c
seventy <sayni1434@gmail.com>
2026-08-23 15:33:43 +0700

    5 задание

--- opp/opp2.py
+++ opp/opp2.py
@@ -26,6 +26,16 @@ class Dog:
         return f"{self.name} лает: Гав-гав!"
 
 
+class Product:
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    def get_total_price(self):
+        return self.price * self.quantity
+
+
+
 
 
 
