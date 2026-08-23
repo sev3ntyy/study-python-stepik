b601bb82a47353589e060ffceb53b5432a923b78
seventy <sayni1434@gmail.com>
2026-08-23 13:59:42 +0700

    выполнил 3 задание

---
+++ opp/opp3.py
@@ -0,0 +1,7 @@
+class TrafficLight:
+    def red_signal(self):
+        return "Стоп"
+    def yellow_signal(self):
+        return "Внимание"
+    def green_signal(self):
+        return "Можно ехать"
