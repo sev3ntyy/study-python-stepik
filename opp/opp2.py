06fec134de9626650bed930dccf19fd4354c80c8
seventy <sayni1434@gmail.com>
2026-08-23 15:29:09 +0700

    3 задание

--- opp/opp2.py
+++ opp/opp2.py
@@ -9,3 +9,14 @@ class Book:
         self.title = title
     def get_title(self):
         return self.title    
+
+
+class User:
+    def __init__(self,username,age):
+        self.username = username 
+        self.age = age
+    def get_info(self):
+        return f"Имя: {self.username}, Возраст: {self.age}"
+
+
+
