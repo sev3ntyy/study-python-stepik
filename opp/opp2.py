ae67df10acfd69ac652670dc2d7e1274885c9bab
seventy <sayni1434@gmail.com>
2026-08-24 18:29:16 +0700

    счетчик объектов

--- opp/opp2.py
+++ opp/opp2.py
@@ -45,6 +45,14 @@ class GameConfig:
     SERVER_NAME = "Stepik-RPG"
 
 
+class Character:
+    character_count = 0 
+    def __init__(self,name):
+        self.name = name
+        Character.character_count = Character.character_count + 1 
+
+
+
 
 
 
