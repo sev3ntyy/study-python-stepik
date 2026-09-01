8f8f1d6bfce009f692b8dc99635654ce405a3d5f
seventy <sayni1434@gmail.com>
2026-09-01 13:02:34 +0700

    wow

--- opp/opp3.py
+++ opp/opp3.py
@@ -29,5 +29,17 @@ class User:
         else:
             self._age = new_age
 
+class Thermostat:
+    def __init__(self, temp):
+        self._temperature = temp
+    def get_temperature(self):
+        return self._temperature
+    def set_temperature(self, new_temp):
+        if 0 <= new_temp <= 100:
+            self._temperature = new_temp
+
+
+
+
 
 
