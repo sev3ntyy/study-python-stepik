f4486b79534516249cf5a0ecc2b3d453a35a15a9
seventy <sayni1434@gmail.com>
2026-09-01 12:56:58 +0700

    2

--- opp/opp3.py
+++ opp/opp3.py
@@ -11,3 +11,10 @@ class LightSwitch:
 
 
 
+class Secret:
+    def __init__(self, secret_message: str):
+        self._message = secret_message
+
+    def get_message(self) -> str:
+        return self._message
+
